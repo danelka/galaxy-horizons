9f277a235c335c5843fcd2c19cd95ff2d655bb05
tkoszhan <tamutdzhin2006@mail.ru>
2024-11-17 23:52:30 +0500

    Profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -15,3 +15,15 @@ function displayUserInfo(user) {
     document.getElementById('userEmail').textContent = user.email || 'N/A';
     document.getElementById('userPhone').textContent = user.phone || 'N/A';
 }
+
+// Function to preview uploaded image
+function previewImage(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('profileImgPreview').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
